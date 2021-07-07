02f3dbc482d351401ea29d5dcd173a918f1fa8bc
Jayamadhu Thisun <41523264+ThisunJay@users.noreply.github.com>
2021-07-07 13:02:24 +0530

    build folder

---
+++ build/precache-manifest.17d740a8ee0216cab807095ab1ba6779.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "850af544051d21dd994478137b55514f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1351b06a501f0e7dab7",
+    "url": "/static/css/2.ce90e6f4.chunk.css"
+  },
+  {
+    "revision": "f8c3f92a7dd381fe27e7",
+    "url": "/static/css/main.73a13a62.chunk.css"
+  },
+  {
+    "revision": "b1351b06a501f0e7dab7",
+    "url": "/static/js/2.f664ceae.chunk.js"
+  },
+  {
+    "revision": "5d587f500f92c6ead6130b7be09ed907",
+    "url": "/static/js/2.f664ceae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8c3f92a7dd381fe27e7",
+    "url": "/static/js/main.1b80bc38.chunk.js"
+  },
+  {
+    "revision": "ab35ce1d95bfaabd0621",
+    "url": "/static/js/runtime-main.bfac319d.js"
+  },
+  {
+    "revision": "0b3ec159c6b1a4f32ea403b19063e217",
+    "url": "/static/media/cod.0b3ec159.jpg"
+  },
+  {
+    "revision": "268ff0715064c2247da2b918e0efa7dd",
+    "url": "/static/media/loading.268ff071.svg"
+  },
+  {
+    "revision": "4a10a4f4189d9dfcc3b5e74946f6e97c",
+    "url": "/static/media/newnew.4a10a4f4.png"
+  },
+  {
+    "revision": "6c4b9e41bbce5a7ad9298430fa2b4272",
+    "url": "/static/media/online.6c4b9e41.png"
+  }
+]);
